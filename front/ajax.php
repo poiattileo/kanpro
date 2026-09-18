d364e3ecb394ca94d93d7760e4b712747bfb2a3b
Leonardo Poiatti Fação <asasdeltasevida@gmail.com>
2026-09-18 10:06:50 -0300

    fix: Componentes exibe apenas DIARIO (ou em branco se vazio)

--- front/ajax.php
+++ front/ajax.php
@@ -1158,9 +1158,9 @@ switch ($action) {
                 'origin_entity_name' => $origin_entity_name,
                 'final_status'       => $status_final,
                 'final_reason'       => $m['diary'] ?? '',
-                'final_components'   => json_encode(['diario' => $m['diary'] ?? '', 'kanpro_model' => $m['model'], 'kanpro_seq' => $m['seq']], JSON_UNESCAPED_UNICODE),
+                'final_components'   => json_encode(!empty(trim($m['diary'] ?? '')) ? ['diario' => trim($m['diary'])] : [], JSON_UNESCAPED_UNICODE),
                 'work_log'           => $m['diary'] ?? '',
-                'work_components'    => json_encode(['kanpro'=>true], JSON_UNESCAPED_UNICODE),
+                'work_components'    => json_encode([], JSON_UNESCAPED_UNICODE),
                 'work_status'        => $work_status,
             ]);
         }
